82875ef12483d42be49d567709fc4981b92cbabe
johnslopez <jcbas99@gmail.com>
2020-07-07 20:10:05 -0400

    files

--- logic.js
+++ logic.js
@@ -1,23 +1,24 @@
 
 function ReturnScheduleDetails() {
+	// get all the data -- x
 
 	//Get Member info
-	var MemberNameValue = document.getElementById('member');
-	var MemberNameValue = MemberNameValue.value;
-	document.getElementById('MemberNameValue').innerHTML= " Member : " + MemberNameValue;
- 
+	var memberName = document.getElementById('member');
+	var memberNameValue = memberName.value;
+	document.getElementById('MemberNameValue').innerHTML = " Member : " + memberNameValue;
+
 	var serviceType = document.getElementById('ServiceType');
 	var serviceTypeValue = serviceType.value;
-   // alert(serviceTypeValue);
-	document.getElementById('ServiceField').innerHTML= "Type of Service: " + serviceTypeValue;
-   // document.getElementById("demo").innerHTML = "Hello World";
+	// alert(serviceTypeValue);
+	document.getElementById('ServiceField').innerHTML = "Type of Service: " + serviceTypeValue;
+	// document.getElementById("demo").innerHTML = "Hello World";
 
-   //Get instructor info
+	//Get instructor info
 	var instructorType = document.getElementById('instructor');
 	var instructorTypeValue = instructorType.value;
-	document.getElementById('InstructorField').innerHTML= "Instructor: " + instructorTypeValue;
+	document.getElementById('InstructorField').innerHTML = "Instructor: " + instructorTypeValue;
 
-	 //Get Month info
+	//Get Month info
 	var monthType = document.getElementById('month');
 	var monthTypeValue = monthType.value;
 	//document.getElementById('MonthField').innerHTML= "MonthField: " + monthTypeValue;
@@ -31,10 +32,23 @@ function ReturnScheduleDetails() {
 	var yearType = document.getElementById('year');
 	var yearTypeValue = yearType.value;
 	//document.getElementById('YearField').innerHTML= "YearField: " + yearTypeValue;
-	document.getElementById("MonthDayYear").innerHTML = "Date: " + monthTypeValue +" " + dayTypeValue + ", " + yearTypeValue;
+	document.getElementById("MonthDayYear").innerHTML = "Date: " + monthTypeValue + " " + dayTypeValue + ", " + yearTypeValue;
 
 	//Get timeslot info
 	var timeSlotType = document.getElementById('timeslot');
 	var timeTypeValue = timeSlotType.value;
-	document.getElementById('TimeField').innerHTML= "Time Slot: " + timeTypeValue;
-  }
+	document.getElementById('TimeField').innerHTML = "Time Slot: " + timeTypeValue;
+
+
+	// 1) create an object w/ all your members data
+	// kinda like below
+
+	let memberSaveData = {
+		name: memberNameValue,
+		service: serviceTypeValue,
+		date: monthTypeValue + '/' + dayTypeValue + '/' + yearTypeValue
+	}
+	localStorage.setItem('memberInformation-' + memberNameValue, JSON.stringify(memberSaveData));
+	}
+	document.querySelector('#myButton1').addEventListener('click', ReturnScheduleDetails)
+
